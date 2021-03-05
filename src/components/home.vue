3eda145f9a28cb4fe3da61b25cf8787562da5673
Timur Khusainov <timur.khusainov@data-brainstorm.com>
2021-03-05 16:48:38 +0100

    firebase init

---
+++ src/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3 v-for="(item, i) in gyroscope" :key="i">
+      {{item}}
+    </h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        gyroscope : "sdf",
+      }
+    },
+    created() {
+      let sensor = new RelativeOrientationSensor({ frequency: 60 });
+      sensor.onreading = (e) => this.gyroscope = e.currentTarget.quaternion
+      sensor.start();
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
