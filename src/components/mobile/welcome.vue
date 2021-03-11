d8866e99c667dcfcb338b03e1c62c443bc0a4ec0
Timur Khusainov <timur.khusainov@data-brainstorm.com>
2021-03-11 13:31:18 +0100

    added new font

--- src/components/mobile/welcome.vue
+++ src/components/mobile/welcome.vue
@@ -1,24 +1,24 @@
 <template>
   <div>
     <v-row class="ma-5 justify-center">
-      <h1>
-        Welcome to gyropaint
+      <h1 class="logo_text">
+        Gyropaint
       </h1>
     </v-row>
     <v-row class="ma-5">
-      <h3>
+      <h4>
         1. Open this site on a big screen like TV or Projector
-      </h3>
-      <h3>
+      </h4>
+      <h4>
         2. Scan QR code on your another screen
-      </h3>
-      <h3>
+      </h4>
+      <h4>
         3. Flip your phone to draw
-      </h3>
+      </h4>
     </v-row>
 
     <v-row class="ma-5 justify-center">
-      <v-btn @click="$emit('toScan')" id="scanQR" color="primary">
+      <v-btn elevation="10" @click="$emit('toScan')" id="scanQR" color="primary">
         <div>
           Scan QR code 
         </div>
@@ -42,4 +42,17 @@
   height: 80vw;
   width: 80vw;
 }
+
+  @font-face {
+    font-family: "Sweet";
+    src: local("Sweet"),
+    url(../../assets/sweet.ttf) format("truetype");
+  }
+ 
+  .logo_text {
+     font-family: "Sweet";
+     font-size: 20vw;
+     text-shadow: 16px 11px 10px rgba(0,0,0,0.6);
+     color: rgba(72, 78, 107, 0.842);
+  }
 </style>
